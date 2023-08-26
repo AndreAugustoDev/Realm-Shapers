bb53c919de3c3918e57c087c64569d65859ca5be
André Augusto <andreaugustoqueiroz999@gmail.com>
2023-08-26 18:55:21 -0300

    initial functional commit

--- common/src/main/java/com/realmshapers/RealmShapers.java
+++ common/src/main/java/com/realmshapers/RealmShapers.java
@@ -3,7 +3,6 @@ package com.realmshapers;
 import com.google.common.base.Suppliers;
 import dev.architectury.registry.CreativeTabRegistry;
 import dev.architectury.registry.registries.DeferredRegister;
-import dev.architectury.registry.registries.Registries;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
@@ -14,19 +13,19 @@ import net.minecraft.world.item.ItemStack;
 import java.util.function.Supplier;
 
 public class RealmShapers {
-    public static final String MOD_ID = "examplemod";
+    public static final String MOD_ID = "realmshapers";
     // We can use this if we don't want to use DeferredRegister
-    public static final Supplier<Registries> REGISTRIES = Suppliers.memoize(() -> Registries.get(MOD_ID));
+    //public static final Supplier<Registries> REGISTRIES = Suppliers.memoize(() -> Registries.get(MOD_ID));
     // Registering a new creative tab
-    public static final CreativeModeTab EXAMPLE_TAB = CreativeTabRegistry.create(new ResourceLocation(MOD_ID, "example_tab"), () ->
-            new ItemStack(RealmShapers.EXAMPLE_ITEM.get()));
+    //public static final CreativeModeTab EXAMPLE_TAB = CreativeTabRegistry.create(new ResourceLocation(MOD_ID, "example_tab"), () ->
+    //        new ItemStack(RealmShapers.EXAMPLE_ITEM.get()));
     
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, Registry.ITEM_REGISTRY);
-    public static final RegistrySupplier<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () ->
-            new Item(new Item.Properties().tab(RealmShapers.EXAMPLE_TAB)));
+    //public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, Registry.ITEM_REGISTRY);
+    //public static final RegistrySupplier<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () ->
+    //        new Item(new Item.Properties().tab(RealmShapers.EXAMPLE_TAB)));
     
     public static void init() {
-        ITEMS.register();
+        //ITEMS.register();
         
         System.out.println(ExampleExpectPlatform.getConfigDirectory().toAbsolutePath().normalize().toString());
     }
